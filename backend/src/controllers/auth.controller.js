8d0cd39a1a2526bd6b3233d9d39ac2002a26c273
pritam <pritamsuryawanshi1121@gmail.com>
2025-05-04 15:08:50 +0530

    4th commit backend user authentication done authenticated/unauthenticated - check

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -138,9 +138,13 @@ export const check = async(req,res)=>{
     try {
         res.status(200).json({
             success:true,
-            message:"User authenticated successfully"
+            message:"User authenticated successfully",
+            user:req.user
         })
     } catch (error) {
-        
+     console.error("User checking user:",error);
+     res.status(500).json({
+        error:"Error checking user"
+     })   
     }
 }
