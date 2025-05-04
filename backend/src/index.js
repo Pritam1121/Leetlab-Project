2af5c69792a4481df12ed18a4b63327c4cd00b7d
pritam <pritamsuryawanshi1121@gmail.com>
2025-05-04 14:24:49 +0530

    3rd commit backend authentication complited user register, user login and user logout done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,25 @@
-import express from 'express' 
-import dotenv from 'dotenv'
+import express from 'express'; 
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes.js';
+
+
 
 dotenv.config(); 
-
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser())
+
 
 const port= process.env.PORT || 8080;
 
-app.get('/',(req,resp)=>{
-    resp.send('Hello')
+app.get('/',(req,res)=>{
+    res.send('Hello, Welcome to leetlab⭐⭐')
 })
+
+app.use("/api/v1/use/auth",authRoutes)
+
 
 app.listen(port,()=>{
 console.log(`server is running on the ${port}`);
